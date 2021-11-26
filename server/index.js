0b27f495de17e71d9f4e2f2a7d5a002874469e59
dev1ninja <devninja.up@gmail.com>
2021-11-27 04:17:44 +0900

    final

--- server/index.js
+++ server/index.js
@@ -18,8 +18,9 @@ app.get('/products/', (req, res) => {
     .pipe(parse({delimiter: ';'}))
     .on('data', function(csvrow) {
       console.log(csvrow);
-      const options = url.parse(csvrow[2]);
-      csvData.push({"id": csvrow[0], "name": csvrow[1], "picture": {"url": csvrow[2], "width": "", "height": ""}});
+      csvData.push(csvrow);
+      // const options = url.parse(csvrow[2]);
+      // csvData.push({"id": csvrow[0], "name": csvrow[1], "picture": {"url": csvrow[2], "width": "", "height": ""}});
       // https.get(options, res => {
       //   const chunks = [];
       //   res.on('data', chunk => {
@@ -35,10 +36,42 @@ app.get('/products/', (req, res) => {
     .on('end',function() {
       //do something with csvData
       console.log('This is csv Data', csvData);
-      csvData.shift();
-      res.send(csvData);
+      // csvData.shift();
+      
+      res.send(getDetails(csvData));
     });
 })
+
+async function getSize(data) {
+  return new Promise((resolve, reject) => {
+    try {
+      const options = url.parse(data[2]);
+      https.get(options, res => {
+        const chunks = [];
+        res.on('data', chunk => {
+          chunks.push(chunk);
+        }).on('end', () => {
+          const buffer = Buffer.concat(chunks);
+          const detail = sizeOf(buffer);
+          // console.log(detail);
+          resolve({"id": data[0], "name": data[1], "picture": {"url": data[2], "width": detail.width, "height": detail.height}});
+        })
+      }) 
+    } catch (error) {
+      resolve(error);
+    }
+  })
+}
+
+async function getDetails(data) {
+  const getList = [];
+  for(let i = 0; i < data.length; i++){
+    getList.push(await getSize(data[i]));
+  }
+  return await Promise.all(getList).then(value => {
+    return value;
+  })
+}
 
 app.post('/upload', (req, res) => {
   console.log('rrrrrrrrr', req);
@@ -47,7 +80,7 @@ app.post('/upload', (req, res) => {
   req.busboy.on('file', function (fieldname, file, filename) {
       console.log("Uploading: " + filename);
 
-      //Path where image will be uploaded
+      //Path where csv will be uploaded
       fstream = fs.createWriteStream(__dirname + '/upload/' + 'products.csv');
       file.pipe(fstream);
       fstream.on('close', function () {    
