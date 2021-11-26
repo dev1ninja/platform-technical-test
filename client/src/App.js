bfc6641091ef167f94fa4e3062b0bb6cbac20df6
dev1ninja <devninja.up@gmail.com>
2021-11-27 03:43:11 +0900

    final

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,83 @@
-import logo from './logo.svg'
 import './App.css'
+import { useState } from 'react'
+import axios from "axios";
 
 function App() {
+  const [mode, setMode] = useState('upload');
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState('');
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    let res = await uploadFile(file);
+    console.log(res.data);
+  };
+
+  const uploadFile = async file => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    console.log('This is csv file', file);
+
+    return await axios.post("http://localhost:5000/upload", formData);
+  };
+
+  const handleOnChange = e => {
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
+  };
+  const showResults = () => {
+    axios.get("http://localhost:5000/products/").then(res => {
+      console.log('data', res.data);
+      setProducts(res.data);
+      setMode('view');
+    }).catch(e=> console.log(e))
+  }
+  const validate = (data) => {
+    if(!data.id || !data.name || !data.picture.url){
+      setError('Error');
+      return false;
+    }
+    return true
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <div style={{flexGrow: 1}}></div>
+        <button>Validate</button>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'center', gap: '20px'}}>
+        <button onClick={() => setMode('upload')}>Products</button>
+        <button onClick={() => showResults()}>Results</button>
+      </div>
+      {mode === 'upload' && <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Products File Upload</h1>
+        <input type="file" onChange={handleOnChange} />
+        <button type="submit">Upload File</button>
+      </form>
+      </div>}
+      {mode === 'view' && <div>
+        {products.map(product => {
+          if (validate(product)) return (
+              <div key={product.id} style={{display: 'flex', justifyContent: 'center', gap: '10px'}}>
+                <span style={{color: 'green'}}>OK</span>
+                <span>{product.id}</span>
+                <span>{product.name}</span>
+                <img src={product.picture.url} />
+              </div>
+            );
+          else return (
+            <div key={product.id} style={{display: 'flex', justifyContent: 'center', gap: '10px'}}>
+              <span style={{color: 'red'}}>ERROR</span>
+              <span>{error}</span>
+            </div>
+          );
+        })}
+      </div>}
     </div>
   )
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,60 @@
 import express from 'express'
 import cors from 'cors'
+import busboy from 'connect-busboy'
+import fs from 'fs-extra'
+import { parse } from 'csv-parse'
+import url from 'url'
+import https from 'https'
+import sizeOf from 'image-size'
 
 const app = express()
 
 app.use(cors())
+app.use(busboy())
 
-app.get('/', (req, res) => {
-  res.send('This is from express.js')
+app.get('/products/', (req, res) => {
+  let csvData=[];
+  fs.createReadStream(__dirname + '/upload/products.csv')
+    .pipe(parse({delimiter: ';'}))
+    .on('data', function(csvrow) {
+      console.log(csvrow);
+      const options = url.parse(csvrow[2]);
+      csvData.push({"id": csvrow[0], "name": csvrow[1], "picture": {"url": csvrow[2], "width": "", "height": ""}});
+      // https.get(options, res => {
+      //   const chunks = [];
+      //   res.on('data', chunk => {
+      //     chunks.push(chunk);
+      //   }).on('end', () => {
+      //     const buffer = Buffer.concat(chunks);
+      //     const detail = sizeOf(buffer);
+      //     console.log(detail);
+      //     csvData.push({"id": csvrow[0], "name": csvrow[1], "picture": {"url": csvrow[2], "width": detail.width, "height": detail.height}});
+      //   })
+      // })
+    })
+    .on('end',function() {
+      //do something with csvData
+      console.log('This is csv Data', csvData);
+      csvData.shift();
+      res.send(csvData);
+    });
+})
+
+app.post('/upload', (req, res) => {
+  console.log('rrrrrrrrr', req);
+  var fstream;
+  req.pipe(req.busboy);
+  req.busboy.on('file', function (fieldname, file, filename) {
+      console.log("Uploading: " + filename);
+
+      //Path where image will be uploaded
+      fstream = fs.createWriteStream(__dirname + '/upload/' + 'products.csv');
+      file.pipe(fstream);
+      fstream.on('close', function () {    
+          console.log("Upload Finished of " + filename);
+          res.send('Success!');
+      });
+  });
 })
 
 const port = process.env.PORT || 5000
